ceea7ac050356375abbdb9886252ef1608ddd0a4
FedorovaElizaveta <147994204+FedorovaElizaveta@users.noreply.github.com>
2024-04-21 10:07:07 +0300

    Added iziToast

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { searchImages } from './js/pixabay-api';
 import { createMarkup } from './js/render-functions';
 
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
@@ -12,30 +15,37 @@ form.addEventListener('submit', handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
 
-  const userValue = event.target.elements.search.value;
+  const userValue = event.target.elements.search.value.trim();
 
-  searchImages(userValue)
-    .then(images => {
-      if (images) {
-        const markup = createMarkup(images);
-
-        gallery.innerHTML = markup;
-
-        const lightbox = new SimpleLightbox('.gallery a', {
-          captionSelector: 'self',
-          captionType: 'data',
-          captionsData: 'caption',
-          captionDelay: 250,
-        });
-
-        lightbox.refresh();
-      } else {
-        gallery.innerHTML = '';
-      }
-    })
-    .catch(error => {
-      console.error(error);
+  if (userValue === '') {
+    iziToast.error({
+      message: 'Please, fill out the field',
     });
+    gallery.innerHTML = '';
+  } else {
+    searchImages(userValue)
+      .then(images => {
+        if (images) {
+          const markup = createMarkup(images);
+
+          gallery.innerHTML = markup;
+
+          const lightbox = new SimpleLightbox('.gallery a', {
+            captionSelector: 'self',
+            captionType: 'data',
+            captionsData: 'caption',
+            captionDelay: 250,
+          });
+
+          lightbox.refresh();
+        } else {
+          gallery.innerHTML = '';
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 
   event.target.reset();
 }
